1d473a23cee46a94d96f0d808de7314f80ae5da7
BasharYaseen <b.yaseen.95@gmail.com>
2024-06-10 22:46:46 +0300

    Add files via upload

---
+++ DiceApp1.3/dice.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, redirect, url_for
+import random
+
+app = Flask(__name__)
+
+# Dice art dictionary
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/roll')
+def roll_dice():
+    dice_number = random.randint(1, 6)
+    dice_face = dice_art[dice_number]
+    return render_template('index.html', result=dice_number, dice_face=dice_face)
+
+if __name__ == '__main__':
+    app.run(debug=True)
